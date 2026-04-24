7583d446cdb5e1aefbc569f55a8cc0202e3f25ab
MartinVillatoros <martinguille17@gmail.com>
2026-04-23 19:49:24 -0600

    cambio de conexion a postgres usando database url y env example

--- internal/db/postgres.go
+++ internal/db/postgres.go
@@ -3,14 +3,18 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 func ConnectPostgres() (*sql.DB, error) {
-	connStr := "host=localhost port=5432 user=postgres password=MartinPSQL dbname=series_tracker_db sslmode=disable"
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		return nil, fmt.Errorf("la variable de entorno DATABASE_URL no está definida")
+	}
 
-	db, err := sql.Open("postgres", connStr)
+	db, err := sql.Open("postgres", databaseURL)
 	if err != nil {
 		return nil, fmt.Errorf("error al abrir la conexión con PostgreSQL: %w", err)
 	}
